1bb71beeab9aca7b136cdfafa21b18d4e00f264d
Christian <csequeira153@gmail.com>
2020-09-21 18:27:20 -0300

    js almost done

--- format.js
+++ format.js
@@ -1,44 +1,50 @@
 let map = [
-    "wwwwwww",  //botões
-    "0000000",
-    "0000000",
-    "0000000",
-    "0000000",
-    "0000000",
-    "0000000",
-]
+  ["w", "w", "w", "w", "w", "w", "w"], //BOTOES
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+];
 
-let jogadores = 1
-let count = 0
-let lineMap
-let columnMap
+let button;
+let players = 0;
+let lineMap;
+let columnMap;
+let currentPosition;
 
-//botões
-
-
-
-
-
-for (let line = 1; line <= 7; line++){
-    lineMap = document.createElement('div')
-    lineMap.setAttribute ('id', 'line'+[line]) 
-    document.getElementById('board').appendChild(lineMap) 
-    for(let column = 1; column <= 7; column++){
-        columnMap = document.createElement('div')
-        columnMap.setAttribute('id', [line]+ ',' +[column])
-        document.getElementById('line'+[line]).appendChild(columnMap)  
-        if(line === 1){
-            columnMap.setAttribute('class', 'button')
-        }
+for (let line = 1; line <= 7; line++) {
+  lineMap = document.createElement("div");
+  lineMap.setAttribute("id", "line" + [line]);
+  document.getElementById("board").appendChild(lineMap);
+  for (let column = 1; column <= 7; column++) {
+    columnMap = document.createElement("div");
+    columnMap.setAttribute("id", [line] + "," + [column]);
+    document.getElementById("line" + [line]).appendChild(columnMap);
+    if (line === 1) {
+      columnMap.setAttribute("class", "button");
     }
+  }
 }
 
-function jogadas() {
-    if(jogadores === 1){
-        //ação de criar o disco no botão acionado
-        //jogadores ++
-    }else{
-        ////ação de criar o disco no botão acionado
-        //jogadores --
+for (let i = 1; i <= 7; i++) {
+  button = document.getElementById(`1,${i}`);
+  button.addEventListener("click", plays);
+}
+
+function plays() {
+  let clickedButton = this.id[2];
+  for (let i = 6; i >= 0; i--) {
+    if (map[i][clickedButton - 1] === 0) {
+      players++;
+      map[i][clickedButton - 1] = players;
+      currentPosition = document.getElementById(`${i + 1},${clickedButton}`);
+      currentPosition.setAttribute("class", "player" + players);
+      break;
     }
+  }
+  if (players === 2) {
+    players = 0;
+  }
 }
