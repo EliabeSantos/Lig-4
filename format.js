a9efacb033f0a038c0bf45327d9292c39c779e99
Eliabe Santos <eliabedodo@gmail.com>
2020-09-21 15:52:24 -0300

    css basico

--- format.js
+++ format.js
@@ -1,3 +1,44 @@
-let newDiv = document.createElement('div')
-let teste2 = document.createElement('div')
-teste2.setAttribute("id", "teste2")
+let map = [
+    "wwwwwww",  //botões
+    "0000000",
+    "0000000",
+    "0000000",
+    "0000000",
+    "0000000",
+    "0000000",
+]
+
+let jogadores = 1
+let count = 0
+let lineMap
+let columnMap
+
+//botões
+
+
+
+
+
+for (let line = 1; line <= 7; line++){
+    lineMap = document.createElement('div')
+    lineMap.setAttribute ('id', 'line'+[line]) 
+    document.getElementById('board').appendChild(lineMap) 
+    for(let column = 1; column <= 7; column++){
+        columnMap = document.createElement('div')
+        columnMap.setAttribute('id', [line]+ ',' +[column])
+        document.getElementById('line'+[line]).appendChild(columnMap)  
+        if(line === 1){
+            columnMap.setAttribute('class', 'button')
+        }
+    }
+}
+
+function jogadas() {
+    if(jogadores === 1){
+        //ação de criar o disco no botão acionado
+        //jogadores ++
+    }else{
+        ////ação de criar o disco no botão acionado
+        //jogadores --
+    }
+}
