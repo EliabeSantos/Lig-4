6ce08443d8d52924bab49d4ee02251e11cd88a24
Christian <csequeira153@gmail.com>
2020-09-22 19:30:31 -0300

    Style added

--- format.js
+++ format.js
@@ -8,7 +8,6 @@ let map = [
   [0, 0, 0, 0, 0, 0, 0],
 ];
 
-
 let button;
 let players = 0;
 let lineMap;
@@ -16,10 +15,14 @@ let columnMap;
 let currentPosition;
 let playerOneVictories = 0;
 let playerTwoVictories = 0;
-arrayPrint()
+let victoryCondition = 0; // VAR TO STOP MOVEMENT AT VICTORY SCREEN
+let victoryButton = document.getElementById("victoryButton");
+victoryButton.addEventListener("click", reset);
+arrayPrint();
+count();
 
-function arrayPrint(){
-  document.getElementById('board').innerHTML = ''
+function arrayPrint() {
+  document.getElementById("board").innerHTML = "";
   for (let line = 1; line <= 7; line++) {
     lineMap = document.createElement("div");
     lineMap.setAttribute("id", "line" + [line]);
@@ -32,7 +35,7 @@ function arrayPrint(){
         columnMap.setAttribute("class", "button");
       }
     }
-}
+  }
 }
 for (let i = 1; i <= 7; i++) {
   button = document.getElementById(`1,${i}`);
@@ -40,123 +43,166 @@ for (let i = 1; i <= 7; i++) {
 }
 
 function plays() {
-  let clickedButton = this.id[2];
-  for (let i = 6; i >= 0; i--) {
-    if (map[i][clickedButton - 1] === 0) {
-      players++;
-      map[i][clickedButton - 1] = players;
-      currentPosition = document.getElementById(`${i + 1},${clickedButton}`);
-      currentPosition.setAttribute("class", "player" + players);
-      break;
+  if (victoryCondition === 0) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let clickedButton = this.id[2];
+    for (let i = 6; i >= 0; i--) {
+      if (map[i][clickedButton - 1] === 0) {
+        players++;
+        map[i][clickedButton - 1] = players;
+        currentPosition = document.getElementById(`${i + 1},${clickedButton}`);
+
+        if (players === 1) {
+          img.src = "img/Gryffindor.png";
+          div.appendChild(img);
+          currentPosition.appendChild(div);
+        }
+        break;
+      }
     }
+    if (players === 2) {
+      img.src = "img/slytherin.png";
+      div.appendChild(img);
+      currentPosition.appendChild(div);
+      players = 0;
+    }
+    winCondition();
   }
-  if (players === 2) {
-    players = 0;
-}
-    winCondition()
 }
 
-function winCondition(){
+function winCondition() {
   // horizontal
-  for(let i = 1; i < map.length; i++){
-    for(let j = 0; j < 4; j++){
-      if(map[i][j] === map[i][j + 1] && map[i][j] === map[i][j + 2] && map[i][j] === map[i][j + 3] && map[i][j] !== 0){
-        if(map[i][j] === 1){
-          playerOneVictories ++
-         victoryText('player one')
-        }
-        else if (map[i][j] === 2){
-          playerTwoVictories ++
-          victoryText('player two')
+  for (let i = 1; i < map.length; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (
+        map[i][j] === map[i][j + 1] &&
+        map[i][j] === map[i][j + 2] &&
+        map[i][j] === map[i][j + 3] &&
+        map[i][j] !== 0
+      ) {
+        if (map[i][j] === 1) {
+          playerOneVictories++;
+          victoryText("player one");
+        } else if (map[i][j] === 2) {
+          playerTwoVictories++;
+          victoryText("player two");
         }
       }
     }
   }
   // vertical
-  for(let i = 1; i < 4; i++){
-    for(let j = 0; j < 7; j++){
-    if(map[i][j] === map[i + 1][j] && map[i][j] === map[i + 2][j] && map[i][j] === map[i + 3][j] && map[i][j] !== 0)
-      if(map[i][j] === 1){
-        playerOneVictories ++
-        victoryText('player one')
-      }
-      else if (map[i][j] === 2){
-        playerTwoVictories ++
-        victoryText('player two')
-      }
+  for (let i = 1; i < 4; i++) {
+    for (let j = 0; j < 7; j++) {
+      if (
+        map[i][j] === map[i + 1][j] &&
+        map[i][j] === map[i + 2][j] &&
+        map[i][j] === map[i + 3][j] &&
+        map[i][j] !== 0
+      )
+        if (map[i][j] === 1) {
+          playerOneVictories++;
+          victoryText("player one");
+        } else if (map[i][j] === 2) {
+          playerTwoVictories++;
+          victoryText("player two");
+        }
     }
   }
   // diagonal
-  for(let i = 1; i < 4; i++){
-    for(let j = 0; j < 4; j++){
-      if(map[i][j] === map[i + 1][j + 1] && map[i][j] === map[i + 2][j + 2] && map[i][j] === map[i + 3][j + 3] && map[i][j] !== 0){
-        if(map[i][j] === 1){
-          playerOneVictories ++
-         victoryText('Player one')
-        }
-        else if (map[i][j] === 2){
-          playerTwoVictories ++
-          victoryText('player two')
+  for (let i = 1; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (
+        map[i][j] === map[i + 1][j + 1] &&
+        map[i][j] === map[i + 2][j + 2] &&
+        map[i][j] === map[i + 3][j + 3] &&
+        map[i][j] !== 0
+      ) {
+        if (map[i][j] === 1) {
+          playerOneVictories++;
+          victoryText("Player one");
+        } else if (map[i][j] === 2) {
+          playerTwoVictories++;
+          victoryText("player two");
         }
       }
     }
   }
   // hexagonal
-  for(let i = 1; i < 4; i++){
-    for(let j = 3; j < 7; j++){
-      if(map[i][j] === map[i + 1][j - 1] && map[i][j] === map[i + 2][j - 2] && map[i][j] === map[i + 3][j - 3] && map[i][j] !== 0){
-        if(map[i][j] === 1){
-          playerOneVictories ++
-          victoryText('Player one')
-        }
-        else if (map[i][j] === 2){
-          playerTwoVictories ++
-          victoryText('player two')
+  for (let i = 1; i < 4; i++) {
+    for (let j = 3; j < 7; j++) {
+      if (
+        map[i][j] === map[i + 1][j - 1] &&
+        map[i][j] === map[i + 2][j - 2] &&
+        map[i][j] === map[i + 3][j - 3] &&
+        map[i][j] !== 0
+      ) {
+        if (map[i][j] === 1) {
+          playerOneVictories++;
+          victoryText("Player one");
+        } else if (map[i][j] === 2) {
+          playerTwoVictories++;
+          victoryText("player two");
         }
       }
     }
   }
 }
 
-function victoryText(p){
-  let victoryText = document.getElementById('victory')
-  victoryText.innerHTML = ''
-  let paragraph = document.createElement('p')
-  paragraph.innerHTML = p + ' victory'
-  victoryText.appendChild(paragraph)
-  count()
+function victoryText(p) {
+  let victoryText = document.getElementById("victoryMessage");
+  victoryText.innerHTML = "";
+  let paragraph = document.createElement("p");
+  paragraph.innerHTML = p;
+  victoryText.appendChild(paragraph);
+
+  //ADD STYLE FROM VICTORY SCREEN
+  victoryButton.classList.remove("hidden");
+  victoryMessage.classList.remove("hidden");
+  document.getElementById("count").classList.add("blur");
+  document.getElementById("main").classList.add("blur");
+  document.getElementById("background").classList.add("blur");
+  
+  // VAR TO STOP MOVEMENT AT VICTORY SCREEN
+  victoryCondition++;
+
+  count();
 }
 
-function count(){
-  let countText = document.getElementById('count')
-  countText.innerHTML = ''
-  let countParagraph = document.createElement('p')
-  countParagraph.innerHTML = playerOneVictories + ' x ' + playerTwoVictories 
-  countText.appendChild(countParagraph)
+function count() {
+  let countText = document.getElementById("count");
+  countText.innerHTML = "";
+  let countParagraph = document.createElement("p");
+  countParagraph.innerHTML = playerOneVictories + " x " + playerTwoVictories;
+  countText.appendChild(countParagraph);
 }
 
-let victoryButton = document.getElementById('victoryButton')
-victoryButton.addEventListener('click', reset)
-
-function reset(){
-  map = []
-  let linha = []
-  for( let i = 0; i < 7; i++) {
+function reset() {
+  map = [];
+  let linha = [];
+  for (let i = 0; i < 7; i++) {
     for (let j = 0; j < 7; j++) {
-      if (i === 0 ) {
-        linha.push("w")
-      }
-      else{
-        linha.push(0)
+      if (i === 0) {
+        linha.push("w");
+      } else {
+        linha.push(0);
       }
     }
-    map.push(linha)
-    linha = []
+    map.push(linha);
+    linha = [];
   }
-  arrayPrint()
+  arrayPrint();
   for (let i = 1; i <= 7; i++) {
     button = document.getElementById(`1,${i}`);
     button.addEventListener("click", plays);
   }
-  
+  // RETURN TO PLAY SCREEN
+  victoryCondition = 0;
+
+  //REMOVE STYLE FROM VICTORY SCREEN
+  victoryButton.classList.add("hidden");
+  victoryMessage.classList.add("hidden");
+  document.getElementById("count").classList.remove("blur");
+  document.getElementById("main").classList.remove("blur");
+  document.getElementById("background").classList.remove("blur");
 }
