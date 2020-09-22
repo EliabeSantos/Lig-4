a0d4d4bc8f65afd2d415bf7cbbde17d410d7787a
Eliabe Santos <eliabedodo@gmail.com>
2020-09-22 13:57:33 -0300

    condicoes de vitoria

--- format.js
+++ format.js
@@ -46,5 +46,55 @@ function plays() {
   }
   if (players === 2) {
     players = 0;
+}
+    winCondition()
+}
+
+function winCondition(){
+  // horizontal
+  for(let i = 1; i < map.length; i++){
+    for(let j = 0; j < 4; j++){
+      if(map[i][j] === map[i][j + 1] && map[i][j] === map[i][j + 2] && map[i][j] === map[i][j + 3] && map[i][j] !== 0){
+        if(map[i][j] === 1){
+          console.log('player one')
+        }
+        else{
+          console.log('player two')
+        }
+      }
+    }
+  }
+  // vertical
+  for(let i = 1; i < 4; i++){
+    for(let j = 0; j < 7; j++){
+    if(map[i][j] === map[i + 1][j] && map[i][j] === map[i + 2][j] && map[i][j] === map[i + 3][j] && map[i][j] !== 0)
+      if(map[i][j] === 1){
+        console.log('player one')
+      }
+      else{
+        console.log('player two')
+      }
+    }
+  }
+  // diagonal
+  for(let i = 1; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      if(map[i][j] === map[i + 1][j + 1] && map[i][j] === map[i + 2][j + 2] && map[i][j] === map[i + 3][j + 3] && map[i][j] !== 0){
+        if(map[i][j] === 1){
+          console.log('player one')
+        }
+        else{
+          console.log('player two')
+        }
+      }
+    }
+  }
+  // hexagonal
+  for(let i = 1; i < 4; i++){
+    for(let j = 3; j < 7; j++){
+      if(map[i][j] === map[i + 1][j - 1] && map[i][j] === map[i + 2][j - 2] && map[i][j] === map[i + 3][j - 3] && map[i][j] !== 0){
+        console.log("diagonal2")
+      }
+    }
   }
 }
